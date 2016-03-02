9f063f2496b64e7c7582dbeea40be6d8aaedb964
Sander Danielsson <sander.danielsson@gmail.com>
2016-03-02 21:44:50 +0100

    Refactoring colors

--- scripts/Colors.js
+++ scripts/Colors.js
@@ -1,11 +1,15 @@
 var gray = "#323232";
 var white = "#ffffff";
 var green = "#A2FF24";
-var black = "#000000";
+var black = "#101010";
+var pink = "#B73070";
+var lightGray = "#e5e5e5"
 
 module.exports = {
   gray: gray,
   white: white,
   green: green,
-  black: black
+  black: black,
+  pink: pink,
+  lightGray: lightGray
 };
